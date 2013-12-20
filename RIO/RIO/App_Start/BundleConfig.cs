23919082ae7881c10207dd3ed4e1b537c1dfa81d
Rafael Heringer <letsrider@gmail.com>
2013-12-20 15:56:49 -0200

    Arrumação do projeto para front-end
    Criação dos bundles;
    Limpeza da Master;

--- RIO/RIO/App_Start/BundleConfig.cs
+++ RIO/RIO/App_Start/BundleConfig.cs
@@ -8,7 +8,19 @@ namespace RIO
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-  
+            
+            //Scripts: VENDOR
+            bundles.Add(new ScriptBundle("~/Bundles/Scripts/Vendor").Include("~/Scripts/Vendor/jquery.*"));
+
+            //Scripts:  Default
+            bundles.Add(new ScriptBundle("~/Bundles/Scripts/Main").Include("~/Scripts/Main.js"));
+
+            //CSS: VENDOR
+            bundles.Add(new StyleBundle("~/Bundles/Styles/Vendor").Include("~/Content/Styles/Bootstrap/style.css"));
+
+            //CSS: Default
+            bundles.Add(new StyleBundle("~/Bundles/Styles/Main").Include("~/Content/Styles/default.css"));
+           
         }
     }
 }
