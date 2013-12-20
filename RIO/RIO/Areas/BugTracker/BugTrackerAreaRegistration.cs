860fda89a66ee89a6f4143014c6c15e5d6c03c28
yslladd <daniel_r_melo@yahoo.com.br>
2013-12-20 17:29:01 -0200

    Estrutura Inicial
    - Criação das áreas TimeSheet, BugTracker

---
+++ RIO/RIO/Areas/BugTracker/BugTrackerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace RIO.Areas.BugTracker
+{
+    public class BugTrackerAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "BugTracker";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "BugTracker_default",
+                "BugTracker/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ RIO/RIO/Areas/TimeSheet/TimeSheetAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace RIO.Areas.TimeSheet
+{
+    public class TimeSheetAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "TimeSheet";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "TimeSheet_default",
+                "TimeSheet/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
