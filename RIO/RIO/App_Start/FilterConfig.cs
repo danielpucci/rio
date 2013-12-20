42f1a168bb2e40971591bc64250b42df4c805af4
Rafael Heringer <letsrider@gmail.com>
2013-12-20 14:45:02 -0200

    PRimeiro COMMIT

---
+++ RIO/RIO/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RIO
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
