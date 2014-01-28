181cb18875ecdbfb944c73655f07f3d38a8c59a0
danielpucci <daniel.pucciariello@gmail.com>
2014-01-28 11:32:14 -0200

    Ajustes no timesheet e inserção de novos plugins.

---
+++ RIO/RIO/Content/Scripts/Timesheet/Index.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    // Máscara nos inputs do timesheet
+    $(".input-sm").mask('00:00');
+
+    // Calendário Dinâmico
+    //var dt = new Date();
+    //var dd = dt.getDate();
+    //var mm = dt.getMonth();
+    //var yyyy = dt.getFullYear();
+
+    //var currentTime = dd + "/" + mm + 1 + "/" + yyyy;
+    //var currentWeekDay = dt.getDay();
+
+    //$(".today").prepend(currentTime);
+    //$(".today small").text(currentWeekDay);
+
+
+
+});
